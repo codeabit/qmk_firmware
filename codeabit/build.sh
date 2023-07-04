73ffba8c1996011c93e5c4445f3a9f204d49474b
Bryan Zimmerman <bryan@codeabit.com>
2023-07-04 10:09:16 -0700

    Remove absolute paths from scripts

--- codeabit/build.sh
+++ codeabit/build.sh
@@ -3,6 +3,10 @@
 
 docker build -t codeabit_qmk_firmware .
 
-docker run --rm -v /Users/bryan/code/qmk_firmware:/root/qmk_firmware codeabit_qmk_firmware 
+cd ..
+SCRIPTDIR=`pwd`
+echo 'Working dir: '$SCRIPTDIR
+
+docker run --rm -v $SCRIPTDIR:/root/qmk_firmware codeabit_qmk_firmware 
 
 mv ../*.hex ./
--- codeabit/qmk_compile.sh
+++ codeabit/qmk_compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /root/qmk_firmware
 /root/.local/bin/qmk compile -kb ferris/sweep_choc_mbuk -km codeabit
-/root/.local/bin/qmk c2json -kb ferris/sweep_choc_mbuk -km codeabit -o ./codeabit/codeabit.json ./keyboards/ferris/keymaps/codeabit/keymap.c
+/root/.local/bin/qmk c2json -kb ferris/sweep -km codeabit -o ./codeabit/codeabit.json ./keyboards/ferris/keymaps/codeabit/keymap.c
