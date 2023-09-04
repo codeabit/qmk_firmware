56f138e6797babe878232d0c7e55d41af994cf5c
Bryan Zimmerman <bryan@codeabit.com>
2023-09-03 21:59:35 -0700

    Enable DYNAMIC_TAPPING_TERM_ENABLE

--- keyboards/ferris/keymaps/codeabit/keymap.c
+++ keyboards/ferris/keymaps/codeabit/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_NO,         KC_NO,               KC_NO,        KC_NO),
 
   [4] = LAYOUT_split_3x5_2(
-    KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,               KC_MPLY,      KC_MPRV,       KC_VOLD,       KC_VOLU,       KC_MNXT, 
+    KC_NO,         DT_DOWN,       DT_PRINT,      DT_UP,         KC_NO,               KC_MPLY,      KC_MPRV,       KC_VOLD,       KC_VOLU,       KC_MNXT, 
     KC_LSFT,       KC_LCTL,       KC_LALT,       KC_LGUI,       KC_NO,               KC_ESC,       KC_MS_L,       KC_MS_U,       KC_MS_D,       KC_MS_R, 
     KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,               KC_MUTE,      KC_WH_L,       KC_WH_U,       KC_WH_D,       KC_WH_R, 
                                                  KC_BTN1,       KC_BTN2,             KC_BTN3,      KC_NO)
